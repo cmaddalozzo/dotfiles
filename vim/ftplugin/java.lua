c27baa81703b7487d879018084faa9c691bb310d
Curtis Maddalozzo <cmaddalozzo@bloomberg.net>
2024-07-01 12:03:36 -0400

    Add Java config

--- vim/ftplugin/java.lua
+++ vim/ftplugin/java.lua
@@ -1,12 +1,111 @@
--- vim.lsp.set_log_level('TRACE')
-local mason_registry = require('mason-registry')
+-- JDTLS (Java LSP) configuration
+local jdtls = require('jdtls')
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+local home = vim.uv.os_homedir()
+local workspace_dir = home .. '/.cache/jdtls/workspace/' .. project_name
+local java_home = home .. '/.sdkman/candidates/java/current'
+
+
+vim.lsp.set_log_level("TRACE")
+
+-- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
 local config = {
+  -- The command that starts the language server
+  -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
   cmd = {
-    mason_registry.get_package('jdtls'):get_install_path() .. '/bin/jdtls',
-    '--data',
-    vim.loop.os_homedir() .. '/.cache/jdtls/workspace',
+    java_home .. '/bin/java',
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-javaagent:' .. home .. '/.local/share/nvim/mason/share/jdtls/lombok.jar',
+    '-Xmx4g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+
+    -- Eclipse jdtls location
+    '-jar', home .. '/.local/share/nvim/mason/share/jdtls/plugins/org.eclipse.equinox.launcher.jar',
+    -- TODO Update this to point to the correct jdtls subdirectory for your OS (config_linux, config_mac, config_win, etc)
+    '-configuration', home .. '/.local/share/nvim/mason/packages/jdtls/config_mac',
+    '-data', workspace_dir
   },
-  root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
+
+  -- This is the default if not provided, you can remove it. Or adjust as needed.
+  -- One dedicated LSP server & client will be started per unique root_dir
+  root_dir = require('jdtls.setup').find_root({ '.git', 'mvnw', 'pom.xml', 'build.gradle' }),
+
+  -- Here you can configure eclipse.jdt.ls specific settings
+  -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+  settings = {
+    java = {
+      -- TODO Replace this with the absolute path to your main java version (JDK 17 or higher)
+      home = java_home,
+      eclipse = {
+        downloadSources = true,
+      },
+      configuration = {
+        updateBuildConfiguration = "interactive",
+      },
+      maven = {
+        downloadSources = true,
+      },
+      implementationsCodeLens = {
+        enabled = true,
+      },
+      referencesCodeLens = {
+        enabled = true,
+      },
+      references = {
+        includeDecompiledSources = true,
+      },
+      signatureHelp = { enabled = true },
+      format = {
+        enabled = true,
+        -- Formatting works by default, but you can refer to a specific file/URL if you choose
+        -- settings = {
+        --   url = "https://github.com/google/styleguide/blob/gh-pages/intellij-java-google-style.xml",
+        --   profile = "GoogleStyle",
+        -- },
+      },
+    },
+    completion = {
+      favoriteStaticMembers = {
+        "org.hamcrest.MatcherAssert.assertThat",
+        "org.hamcrest.Matchers.*",
+        "org.hamcrest.CoreMatchers.*",
+        "org.junit.jupiter.api.Assertions.*",
+        "java.util.Objects.requireNonNull",
+        "java.util.Objects.requireNonNullElse",
+        "org.mockito.Mockito.*",
+      },
+      importOrder = {
+        "java",
+        "javax",
+        "com",
+        "org"
+      },
+    },
+    extendedClientCapabilities = jdtls.extendedClientCapabilities,
+    sources = {
+      organizeImports = {
+        starThreshold = 9999,
+        staticStarThreshold = 9999,
+      },
+    },
+    codeGeneration = {
+      toString = {
+        template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+      },
+      useBlocks = true,
+    },
+  },
+  -- Needed for auto-completion with method signatures and placeholders
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
+  flags = {
+    allow_incremental_sync = true,
+  },
 }
-require('jdtls').start_or_attach(config)
-require('jdtls').start_or_attach(config)
+-- This starts a new client & server, or attaches to an existing client & server based on the `root_dir`.
+jdtls.start_or_attach(config)
